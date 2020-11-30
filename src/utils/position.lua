572443c206f1fc0e043de55fe4d7b455f912e9cb
speakk <speakk@gmail.com>
2020-11-30 09:42:51 +0200

    Commented code cleanup

--- src/utils/position.lua
+++ src/utils/position.lua
@@ -34,28 +34,5 @@ return {
   end,
   getPhysicsBodyTransform = function(entity)
     return getTransform(entity.physicsBody, entity.sprite)
-    -- local body = entity.physicsBody
-    -- if entity.sprite then
-    --   local mediaEntity = mediaManager:getMediaEntity(entity.sprite.spriteId)
-    --   local quad = mediaEntity.quads[entity.sprite.currentQuadIndex or 1]
-    --   local _, _, quadW, quadH = quad:getViewport()
-    --   local w = quadW * body.width
-    --   local h = quadH * body.height
-
-    --   local originX, originY = mediaEntity.origin.x, mediaEntity.origin.y
-
-    --   local originXpx = -originX * quadW
-    --   local centerX = originXpx + body.offsetX * quadW
-    --   local x = centerX - w/2
-
-    --   local originYpx = -originY * quadW
-    --   local centerY = originYpx + body.offsetY * quadW
-    --   local y = centerY - w/2
-
-    --   return x, y, w, h
-    -- end
-
-    -- -- TODO: Replace 32 with tilesize
-    -- return 0, 0, body.pixelWidth or 32, body.pixelHeight or 32
   end
 }
